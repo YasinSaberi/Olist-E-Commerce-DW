91e30d2b9e73d209b010b9a7f332fc5fd2b25410
Yasin Saberi <yassaberi97@gmail.com>
2026-06-30 18:12:44 +0330

    feat: add fact_sales table for sales mart

---
+++ sql/03_dw_facts/01_fact_sales.sql
@@ -0,0 +1,22 @@
+USE Olist_DW;
+GO
+
+IF OBJECT_ID('dbo.fact_sales', 'U') IS NOT NULL
+    DROP TABLE dbo.fact_sales;
+GO
+
+CREATE TABLE dbo.fact_sales (
+    sales_sk INT IDENTITY(1,1) PRIMARY KEY,
+    order_id VARCHAR(50) NOT NULL,
+    order_item_id INT NOT NULL,
+    customer_sk INT NOT NULL,
+    product_sk INT NOT NULL,
+    order_date_sk INT NOT NULL,
+    price DECIMAL(10, 2) NULL,
+    freight_value DECIMAL(10, 2) NULL,
+    
+    CONSTRAINT FK_FactSales_Customer FOREIGN KEY (customer_sk) REFERENCES dbo.dim_customer(customer_sk),
+    CONSTRAINT FK_FactSales_Product FOREIGN KEY (product_sk) REFERENCES dbo.dim_product(product_sk),
+    CONSTRAINT FK_FactSales_Date FOREIGN KEY (order_date_sk) REFERENCES dbo.dim_date(date_sk)
+);
+GO
