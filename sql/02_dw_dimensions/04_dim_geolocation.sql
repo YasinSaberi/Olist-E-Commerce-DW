32417f973baa3030fc125ab35e650774d3dd8f77
Yasin Saberi <139574694+YasinSaberi@users.noreply.github.com>
2026-07-04 11:38:35 +0330

    Create dim_seller table and drop if exists

--- sql/02_dw_dimensions/04_dim_geolocation.sql
+++ sql/02_dw_dimensions/04_dim_geolocation.sql
@@ -1,8 +1,14 @@
-CREATE TABLE DimGeolocation (
-    GeoKey INT IDENTITY(1,1) PRIMARY KEY,
-    ZipCodePrefix INT,
-    City VARCHAR(100),
-    State VARCHAR(50),
-    Latitude FLOAT,
-    Longitude FLOAT
+USE Olist_DW;
+GO
+
+IF OBJECT_ID('dbo.dim_seller', 'U') IS NOT NULL
+    DROP TABLE dbo.dim_seller;
+GO
+
+CREATE TABLE dbo.dim_seller (
+    seller_sk INT IDENTITY(1,1) PRIMARY KEY,
+    seller_id VARCHAR(50) NOT NULL,
+    seller_city VARCHAR(100) NULL,
+    seller_state VARCHAR(50) NULL
 );
+GO
