d48742f49893971bd26136ad15bccf3b36c8a368
Yasin <yassabri97@gmail.com>
2026-07-07 07:36:58 +0330

    final changes on develop branch

--- sql/04_etl_pipelines/01_load_dim_customer_scd2.sql
+++ sql/04_etl_pipelines/01_load_dim_customer_scd2.sql
@@ -4,6 +4,8 @@ GO
 CREATE OR ALTER PROCEDURE dbo.sp_load_dim_customer_scd2
 AS
 BEGIN
+    SET NOCOUNT ON; -- Prevents network spam from row counts
+
     -- 1. Create a table variable to temporarily hold the SCD2 updates in memory
     DECLARE @UpdatedRecords TABLE (
         action_name NVARCHAR(10),
@@ -14,32 +16,49 @@ BEGIN
         customer_state NVARCHAR(50)
     );
 
-    -- 2. Perform the MERGE and output the results into the table variable
-    MERGE dbo.dim_customer AS target
-    USING Olist_Staging.dbo.stg_customers AS source
-    ON (target.customer_id = source.customer_id)
-    
-    WHEN MATCHED AND target.IsCurrent = 1 AND (
-        target.customer_zip_code_prefix <> source.customer_zip_code_prefix OR
-        target.customer_city <> source.customer_city OR
-        target.customer_state <> source.customer_state
-    ) THEN 
-        UPDATE SET target.IsCurrent = 0, target.ValidTo = GETDATE()
-        
-    WHEN NOT MATCHED BY TARGET THEN 
-        INSERT (customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state, IsCurrent, ValidFrom, ValidTo)
-        VALUES (source.customer_id, source.customer_unique_id, source.customer_zip_code_prefix, source.customer_city, source.customer_state, 1, GETDATE(), NULL)
-        
-    OUTPUT $action, source.customer_id, source.customer_unique_id, source.customer_zip_code_prefix, source.customer_city, source.customer_state
-    INTO @UpdatedRecords (action_name, customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state);
+    BEGIN TRY
+        -- Start the transaction. If anything fails after this point, the entire block is undone.
+        BEGIN TRAN;
 
-    -- 3. Safely insert the new active records from memory into the dimension
-    INSERT INTO dbo.dim_customer (
-        customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state, IsCurrent, ValidFrom, ValidTo
-    )
-    SELECT 
-        customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state, 1, GETDATE(), NULL
-    FROM @UpdatedRecords
-    WHERE action_name = 'UPDATE';
+        -- 2. Perform the MERGE and output the results into the table variable
+        MERGE dbo.dim_customer AS target
+        USING Olist_Staging.dbo.stg_customers AS source
+        ON (target.customer_id = source.customer_id)
+        
+        WHEN MATCHED AND target.IsCurrent = 1 AND (
+            target.customer_zip_code_prefix <> source.customer_zip_code_prefix OR
+            target.customer_city <> source.customer_city OR
+            target.customer_state <> source.customer_state
+        ) THEN 
+            UPDATE SET target.IsCurrent = 0, target.ValidTo = GETDATE()
+            
+        WHEN NOT MATCHED BY TARGET THEN 
+            INSERT (customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state, IsCurrent, ValidFrom, ValidTo)
+            VALUES (source.customer_id, source.customer_unique_id, source.customer_zip_code_prefix, source.customer_city, source.customer_state, 1, GETDATE(), NULL)
+            
+        OUTPUT $action, source.customer_id, source.customer_unique_id, source.customer_zip_code_prefix, source.customer_city, source.customer_state
+        INTO @UpdatedRecords (action_name, customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state);
+
+        -- 3. Safely insert the new active records from memory into the dimension
+        INSERT INTO dbo.dim_customer (
+            customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state, IsCurrent, ValidFrom, ValidTo
+        )
+        SELECT 
+            customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state, 1, GETDATE(), NULL
+        FROM @UpdatedRecords
+        WHERE action_name = 'UPDATE';
+
+        -- If the engine reaches this line, everything worked. Save it permanently.
+        COMMIT TRAN;
+
+    END TRY
+    BEGIN CATCH
+        -- If any error occurs, destroy the partial transaction to protect the database.
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRAN;
+        
+        -- Throw the error back to SSMS so you can see why it failed.
+        THROW;
+    END CATCH;
 END;
 GO
--- sql/04_etl_pipelines/03_load_fact_sales.sql
+++ sql/04_etl_pipelines/03_load_fact_sales.sql
@@ -4,21 +4,48 @@ GO
 CREATE OR ALTER PROCEDURE dbo.sp_load_fact_sales
 AS
 BEGIN
-    INSERT INTO dbo.fact_sales (order_id, order_item_id, customer_sk, product_sk, order_date_sk, price, freight_value)
-    SELECT 
-        oi.order_id,
-        oi.order_item_id,
-        ISNULL(c.customer_sk, -1),
-        ISNULL(p.product_sk, -1),
-        CAST(FORMAT(o.order_purchase_timestamp, 'yyyyMMdd') AS INT),
-        oi.price,
-        oi.freight_value
-    FROM Olist_Staging.dbo.stg_order_items oi
-    LEFT JOIN Olist_Staging.dbo.stg_orders o 
-        ON oi.order_id = o.order_id
-    LEFT JOIN dbo.dim_customer c 
-        ON o.customer_id = c.customer_id AND c.IsCurrent = 1
-    LEFT JOIN dbo.dim_product p 
-        ON oi.product_id = p.product_id;
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRAN;
+
+        INSERT INTO dbo.fact_sales (
+            order_id, 
+            order_item_id, 
+            customer_sk, 
+            product_sk, 
+            order_date_sk, 
+            price, 
+            freight_value
+        )
+        SELECT 
+            oi.order_id,
+            oi.order_item_id,
+            c.customer_sk,
+            p.product_sk,
+            CAST(CONVERT(VARCHAR(8), o.order_purchase_timestamp, 112) AS INT) AS order_date_sk,
+            oi.price,
+            oi.freight_value
+        FROM Olist_Staging.dbo.stg_order_items oi
+        INNER JOIN Olist_Staging.dbo.stg_orders o 
+            ON oi.order_id = o.order_id
+        INNER JOIN dbo.dim_customer c 
+            ON o.customer_id = c.customer_id AND c.IsCurrent = 1
+        INNER JOIN dbo.dim_product p 
+            ON oi.product_id = p.product_id -- SCD Type 3 does not use IsCurrent
+        WHERE NOT EXISTS (
+            SELECT 1 
+            FROM dbo.fact_sales f 
+            WHERE f.order_id = oi.order_id 
+              AND f.order_item_id = oi.order_item_id 
+        );
+
+        COMMIT TRAN;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRAN;
+        THROW;
+    END CATCH;
 END;
 GO
